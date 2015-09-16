7f1e7fe5308b15c0e0f69eb6bb9ce866b6099326
Kathleen Kent <lashkath@gmail.com>
2015-09-16 15:52:24 -0700

    added updates to index and user routes

--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+router.get('/', function(req, res, next) {
+	res.redirect('/login');
+});
+
 router.get('/login', function(req, res, next) {
-	res.render('index');
-	res.clearCookie('user');
+	res.render('index')
 });
 
 router.post('/login', function(req, res, next) {
 	var name = req.body.username;
 	res.cookie('user', name)
-	//res.end()
-	//console.log(req)
 	res.render('users')
-})
-
+});
 
 router.get('/posts', function(req, res){
 	res.json({posts: req.app.locals.messages});
